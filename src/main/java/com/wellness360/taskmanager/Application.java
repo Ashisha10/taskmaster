eb21b2a22d195baa254703c9a996c5cc50a71834
Ashisha Tulsian <ashisha@Ashishas-MacBook-Air.local>
2025-01-16 23:35:07 +0530

    prom setup and dockerization

--- src/main/java/com/wellness360/taskmanager/Application.java
+++ src/main/java/com/wellness360/taskmanager/Application.java
@@ -3,8 +3,10 @@ package com.wellness360.taskmanager;
 import com.wellness360.taskmanager.controller.TaskController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+//@ComponentScan(basePackages = {"com.wellness360.taskmanager.controller", "com.wellness360.taskmanager.service"})
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
